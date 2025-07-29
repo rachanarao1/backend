234fe4cd15428776214eca15314811d29a459726
rachanarao1 <150130875+rachanarao1@users.noreply.github.com>
2025-07-29 18:53:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ✅ Enable CORS for your GitHub Pages domain
 app.use(cors({
-  origin: "https://github.com/rachanarao1.io/trigger"
+  origin: "https://github.com/rachanarao1.io/"
 }));
 
 app.use(express.json());
